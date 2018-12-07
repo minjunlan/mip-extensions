e4980b0ace2bb40a9eba243d7f3dd7e08ab02453
minjunlan <284517207@qq.com>
2018-12-07 14:53:04 +0800

    init

---
+++ mip-lan-searchbar/mip-lan-searchbar.js
@@ -0,0 +1,18 @@
+/**
+ * @file mip-lan-searchbar 组件
+ * @author
+ */
+
+define(function (require) {
+    'use strict';
+
+    var customElement = require('customElement').create();
+
+    /**
+     * 第一次进入可视区回调，只会执行一次
+     */
+    customElement.prototype.firstInviewCallback = function () {
+    };
+
+    return customElement;
+});
